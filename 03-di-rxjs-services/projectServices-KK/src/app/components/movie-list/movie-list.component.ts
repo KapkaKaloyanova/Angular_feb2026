42022ceda0580974b1582c8a405eea03f586e1a8
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-18 17:27:35 +0200

    adding averageRating$
    Adding averageRating$ calculation and displaying only the movies with this ratings. Using shareReplay() and combineLatest()

--- 03-di-rxjs-services/projectServices-KK/src/app/components/movie-list/movie-list.component.ts
+++ 03-di-rxjs-services/projectServices-KK/src/app/components/movie-list/movie-list.component.ts
@@ -1,30 +1,46 @@
 import { Component, inject } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
-import { map, tap } from 'rxjs';
-import { AsyncPipe, UpperCasePipe } from '@angular/common';
-import { Movie } from '../../services/movie.service';
+import { combineLatest, concatMap, from, map, reduce, shareReplay, tap } from 'rxjs';
+import { AsyncPipe, UpperCasePipe, DecimalPipe } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-movie-list',
-  imports: [AsyncPipe, UpperCasePipe],
+  imports: [AsyncPipe, UpperCasePipe, DecimalPipe],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css',
 })
 export class MovieListComponent {
   private movieService = inject(MovieService);
 
-  movies$ = this.movieService.getMovies().pipe(
-  tap(data => console.log('Raw data:', data)),
-  map(movies => movies
-    .map(m => ({
-      ...m,
-      title: '🎬 ' + m.title,
-      rating: Math.floor(Math.random() * 10) + 1 // Генерираме случаен рейтинг за теста
-    }))
-    .filter(m => m.rating > 8) // Филтрираме тези с рейтинг над 5
-  )
-);
+  sharedMovies$ = this.movieService.getMovies().pipe(
+    map(movies => movies
+      .map(m => ({
+        ...m,
+        title: '🎬 ' + m.title,
+        rating: Math.floor(Math.random() * 10) + 1 // Генерираме случаен рейтинг за теста
+      }))),
+    shareReplay(1)
+  );
 
+  averageRating$ = this.sharedMovies$.pipe(
+    concatMap(movies => from(movies)),
+    reduce((acc, curr: any) => ({
+      sum: acc.sum + curr.rating,
+      count: acc.count + 1
+    }), { sum: 0, count: 0 }),
+    map(res => res.count > 0 ? res.sum / res.count : 0),
+    shareReplay(1)// Изчисляваме средния рейтинг
+  );
+
+  movies$ = combineLatest([this.sharedMovies$, this.averageRating$]).pipe(
+    map(([movies, avg]) => {
+      const min = Math.floor(avg);
+      const max = Math.ceil(avg);
+
+      console.log(`Филтрираме около средно: ${avg.toFixed(2)} (диапазон ${min}-${max})`);
+
+      return movies.filter((m: any) => m.rating >= min && m.rating <= max);
+    }));
 }
