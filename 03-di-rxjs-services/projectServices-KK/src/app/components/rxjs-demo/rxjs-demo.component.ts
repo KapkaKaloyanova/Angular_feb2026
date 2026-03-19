3c6f1ea3161c95b828cb4c381ab1e54e5a1b3ba7
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-19 19:57:59 +0200

    small updates

--- 03-di-rxjs-services/projectServices-KK/src/app/components/rxjs-demo/rxjs-demo.component.ts
+++ 03-di-rxjs-services/projectServices-KK/src/app/components/rxjs-demo/rxjs-demo.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './rxjs-demo.component.css',
 })
 export class RxjsDemoComponent {
+  results: string[] = [];
+
+  clearResults(): void {
+    this.results = [];
+  }
+
+  demo1(): void {
+    this.clearResults();
+    this.results.push('of() + subscribe() demonstration');
+
+    of(1,2,3,4,)
+  }
 
 }
