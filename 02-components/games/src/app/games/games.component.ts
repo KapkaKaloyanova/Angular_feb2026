5673120419471f6c913a2c8741a50d0b89b57a4a
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-19 13:53:52 +0200

    interface game created

--- 02-components/games/src/app/games/games.component.ts
+++ 02-components/games/src/app/games/games.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Game } from '../models/game.model';
+import { GameCardComponent } from '../game-card/game-card.component';
+
 
 @Component({
   selector: 'app-games',
-  imports: [],
+  imports: [GameCardComponent],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css',
 })
 export class GamesComponent {
+  games: Game[] = [];
 
+  ngOnInit() {
+    console.log('Games Component is created');
+
+    this.games = [
+      {
+        id: 1,
+        title: 'The Legend of Zelda',
+        price: 1200,
+        imageUrl: 'https://placehold.co/200x150/3f51b5/white?text=Zelda',
+      },
+      {
+        id: 2,
+        title: 'Super Mario Bros',
+        price: 60,
+        imageUrl: 'https://placehold.co/200x150/e91e63/white?text=Mario',
+      },
+      {
+        id: 3,
+        title: 'Pokemon Violet',
+        price: 45,
+        imageUrl: 'https://placehold.co/200x150/9c27b0/white?text=Pokemon',
+      },
+      {
+        id: 4,
+        title: 'Animal Crossing',
+        price: 80,
+        imageUrl: 'https://placehold.co/200x150/4caf50/white?text=AC',
+      }
+    ]
+
+  }
+
+  ngOnDestroy() {
+    console.log('Games Component is destroyed');
+  }
 }
