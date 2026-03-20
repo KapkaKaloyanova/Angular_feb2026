1c775fdbb9ed00323411cada7b9802b66005f090
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-20 23:47:51 +0200

    Add Nav component and refactor App bootstrap
    Introduce a new layout/nav component (TS, HTML, CSS) and replace the legacy App implementation with an AppComponent-based structure.

--- 05-Modules-signals-and-routing/project-signals-routing/src/app/app.component.ts
+++ 05-Modules-signals-and-routing/project-signals-routing/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from '../layout/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
