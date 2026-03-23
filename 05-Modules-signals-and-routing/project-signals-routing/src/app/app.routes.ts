d30c37310060f113020a0843c8a9f6ab79bcfaa1
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-23 09:50:36 +0200

    add not-found component

--- 05-Modules-signals-and-routing/project-signals-routing/src/app/app.routes.ts
+++ 05-Modules-signals-and-routing/project-signals-routing/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MissionsComponent } from './features/missions/missions.component';
 import { CountdownComponent } from './features/countdown/countdown.component';
 import { LoginComponent } from './features/login/login.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'missions', pathMatch: 'full' },
     { path: 'missions', component: MissionsComponent },
     { path: 'countdown', component: CountdownComponent },
     { path: 'login', component: LoginComponent },
+    { path: '**', component: NotFoundComponent}
 ];
