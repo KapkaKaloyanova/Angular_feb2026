62ea4c34716d3032110e585fe364486d2278eae0
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-23 14:01:58 +0200

    Add mission details component and routing

--- 05-Modules-signals-and-routing/project-signals-routing/src/app/app.routes.ts
+++ 05-Modules-signals-and-routing/project-signals-routing/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MissionsComponent } from './features/missions/missions.component';
 import { CountdownComponent } from './features/countdown/countdown.component';
 import { LoginComponent } from './features/login/login.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
+import { MissionDetailsComponent } from './features/mission-details/mission-details.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'missions', pathMatch: 'full' },
     { path: 'missions', component: MissionsComponent },
+    { path: 'missions/:id', component: MissionDetailsComponent },
     { path: 'countdown', component: CountdownComponent },
     { path: 'login', component: LoginComponent },
     { path: '**', component: NotFoundComponent}
