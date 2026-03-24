e32e1dfaf0d023e331aea8fa8bee52354a4f6eff
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-24 15:19:13 +0200

    Add auth routes and component imports
    Import RegisterComponent and LoginComponent and add routes for '/register' and '/login' to the app routing configuration.

--- KK-workshop/kk-workshop/src/app/app.routes.ts
+++ KK-workshop/kk-workshop/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ThemesComponent } from './features/themes/themes.component';
 import { NewThemeComponent } from './features/themes/new-theme/new-theme.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
 
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    
     { path: 'themes', component: ThemesComponent },
     { path: 'themes/new', component: NewThemeComponent },
 
