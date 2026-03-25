cd7ce6642b7c47827a6a8413710852d11ccfcd28
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-25 16:16:28 +0200

    Add ProfileEditComponent

---
+++ KK-workshop/kk-workshop/src/app/features/profile-edit/profile-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-edit',
+  imports: [RouterLink],
+  templateUrl: './profile-edit.component.html',
+  styleUrl: './profile-edit.component.css',
+})
+export class ProfileEditComponent {
+
+}
