7b26a670bb8bcd12bf6501d9cfe12fc9208b4e21
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-03-28 01:29:16 +0200

    Refactor new-theme component

--- KK-workshop/kk-workshop/src/app/features/themes/new-theme/new-theme.component.ts
+++ KK-workshop/kk-workshop/src/app/features/themes/new-theme/new-theme.component.ts
@@ -1,27 +1,48 @@
-import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { InputErrorDirective } from '../../../shared/directives/input-error.directive';
 import { ApiService } from '../../../core/services/api.service';
-import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-new-theme',
-  imports: [FormsModule],
+  imports: [FormsModule,  InputErrorDirective],
   templateUrl: './new-theme.component.html',
   styleUrl: './new-theme.component.css',
 })
 export class NewThemeComponent {
+  @ViewChild('themeForm') themeForm!: NgForm;
   private router = inject(Router);
+  private apiService = inject(ApiService);
 
   themeName = '';
   postText = '';
-  
-  onSubmit():void {
-    console.log('Creating theme', this.themeName, this.postText);
-    this.router.navigate(['/themes']);
-  }
+  isLoading = false;
+  errorMessage = '';
 
-  onCancel():void {
+  onSubmit(): void {
+    if (this.themeForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.apiService.createTheme({
+      themeName: this.themeName,
+      postText: this.postText
+    }).subscribe({
+      next: (theme) => {
+        this.isLoading = false;
+        this.router.navigate(['/themes', theme._id]);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.message || 'Failed to create a new theme. Try again';
+      }
+    })
+  }
+  onCancel(): void {
     this.router.navigate(['/home']);
-  } 
+  }
 }
